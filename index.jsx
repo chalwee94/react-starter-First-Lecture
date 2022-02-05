1d726639a2b87d3b12682256499a0cb3987eb376
patrick yoro <patrick.yoro@yahoo.no>
2022-02-05 02:43:21 +0100

    working on first lecture

--- index.jsx
+++ index.jsx
@@ -1,4 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("app"));
+function FrontPage() {
+    return <div>
+        <h1>my react movie</h1>
+        <ul>
+            <li><Link to="/movies">List movies</Link></li>
+            <li><Link to="/movies/new">New Movie</Link></li>
+        </ul>
+    </div>;
+}
+
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/movies/new" element={<h1>New movie</h1>}/>
+            <Route path="/movies" element={<h1>List movies</h1>}/>
+        </Routes>
+    </BrowserRouter>;
+}
+
+
+
+
+ReactDOM.render(
+    <Application/>,
+    document.getElementById("app")
+
+);
