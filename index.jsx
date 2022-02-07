99fb489673840b7c4980d409d9397e6cc59addaa
patrick yoro <patrick.yoro@yahoo.no>
2022-02-07 01:39:46 +0100

    fixed som issue

--- index.jsx
+++ index.jsx
@@ -1,24 +1,24 @@
 import * as React from "react";
+import {useState} from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
-
-const Movies = [
+const MOVIES = [
     {
-        title: "De urørlige",
-        plot: " De urørlige er en oppløftende komedie om vennskap, tillit og menneskelige muligheter, og er den sanne historien om to menn som aldri skulle ha møtt hverandre.",
-        year: 2011
+        title: "When They See Us",
+        plot: " is a 2019 American crime drama television miniseries created, co-written, and directed by Ava DuVernay for Netflix, that premiered in four parts on May 31, 2019. It is based on events of the 1989 Central Park jogger case and explores the lives and families of the five Black and Latino male suspects who were falsely accused then prosecuted on charges related to the rape and assault of a white woman in Central Park, New York City. The series features an ensemble cast, including Jharrel Jerome, Asante Blackk, Jovan Adepo, Michael K. Williams, Logan Marshall-Green, Joshua Jackson, Blair Underwood, Vera Farmiga, John Leguizamo, Felicity Huffman, Niecy Nash, Aunjanue Ellis, Marsha Stephanie Blake, and Kylie Bunbury",
+        year: 2019
     },
     {
-        title: "The Color Purple",
-        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
-        year: 1985
+        title: "De urørlige",
+        plot: "De urørlige er en oppløftende komedie om vennskap, tillit og menneskelige muligheter, og er den sanne historien om to menn som aldri skulle ha møtt hverandre.",
+        year: 2011
+
     }
 ];
-
 function FrontPage() {
     return <div>
-        <h1>my react movie</h1>
+        <h1>my react movies</h1>
         <ul>
             <li><Link to="/movies">List movies</Link></li>
             <li><Link to="/movies/new">New Movie</Link></li>
@@ -27,32 +27,54 @@ function FrontPage() {
 }
 function ListMovies({movies}) {
     return <div>
-        <h1>List Movies</h1>
-        {Movies.map(m =>
-            <>
+        <h1>List movies</h1>
+        {movies.map(m =>
             <div key={m.title}>
                 <h2>{m.title} ({m.year})</h2>
                 <div>{m.plot}</div>
             </div>
-            </>
-
         )}
     </div>;
+}
+
+
+function Newmovie() {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        MOVIES.push({title, year, plot});
+}
+    return <form onSubmit={handleSubmit}>
+        <h1>New movie</h1>
+        <div>
+            <label>Title: <input value={title} onChange={e => setTitle(e.target.value)} /></label>
+        </div>
+        <div>
+            <label>Year: <input value={year} onChange={e => setYear(e.target.value)} /></label>
+        </div>
+        <div>
+            <label>Plot: <textarea value={plot} onChange={e => setPlot(e.target.value)} /></label>
+        </div>
+        <button>Submit</button>
+        <pre>
+            {JSON.stringify({title, year, plot})}
+        </pre>
+    </form>;
 }
 
 function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage/>}/>
-            <Route path="/movies/new" element={<h1>New movie</h1>}/>
-            <Route path="/movies" element={<ListMovies/>}/>
-            <Route path="/movies" element={<ListMovies movies={Movies}/>}/>
+            <Route path="/movies/new" element={<Newmovie/>}/>
+            <Route path="/movies" element={<ListMovies movies={MOVIES}/>}/>
         </Routes>
     </BrowserRouter>;
 }
-
 ReactDOM.render(
     <Application/>,
     document.getElementById("app")
-
 );
