a6dff48cd2c7ec200954f6ac930c24ee787a8074
patrick yoro <patrick.yoro@yahoo.no>
2022-02-09 22:40:44 +0100

    working on my project

--- index.jsx
+++ index.jsx
@@ -1,25 +1,25 @@
 import * as React from "react";
-import {useState} from "react";
 import {useState, useEffect} from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom";
 
+
+
 const MOVIES = [
     {
-        title: "When They See Us",
-        plot: " is a 2019 American crime drama television miniseries created, co-written, and directed by Ava DuVernay for Netflix, that premiered in four parts on May 31, 2019. It is based on events of the 1989 Central Park jogger case and explores the lives and families of the five Black and Latino male suspects who were falsely accused then prosecuted on charges related to the rape and assault of a white woman in Central Park, New York City. The series features an ensemble cast, including Jharrel Jerome, Asante Blackk, Jovan Adepo, Michael K. Williams, Logan Marshall-Green, Joshua Jackson, Blair Underwood, Vera Farmiga, John Leguizamo, Felicity Huffman, Niecy Nash, Aunjanue Ellis, Marsha Stephanie Blake, and Kylie Bunbury",
-        year: 2019
-    },
-    {
-        title: "De urørlige",
+        title: "De urølige ",
         plot: "De urørlige er en oppløftende komedie om vennskap, tillit og menneskelige muligheter, og er den sanne historien om to menn som aldri skulle ha møtt hverandre.",
         year: 2011
-
+    },
+    {
+        title: "When They See Us",
+        plot: "When They See Us is a 2019 American crime drama television miniseries created, co-written, and directed by Ava DuVernay for Netflix, that premiered in four parts on May 31, 2019. It is based on events of the 1989 Central Park jogger case and explores the lives and families of the five Black and Latino male suspects who were falsely accused then prosecuted on charges related to the rape and assault of a white woman in Central Park, New York City. The series features an ensemble cast, including Jharrel Jerome, Asante Blackk, Jovan Adepo, Michael K. Williams, Logan Marshall-Green, Joshua Jackson, Blair Underwood, Vera Farmiga, John Leguizamo, Felicity Huffman, Niecy Nash, Aunjanue Ellis, Marsha Stephanie Blake, and Kylie Bunbury.",
+        year: 2019
     }
 ];
 function FrontPage() {
     return <div>
-        <h1>my react movies</h1>
+        <h1>my react movies </h1>
         <ul>
             <li><Link to="/movies">List movies</Link></li>
             <li><Link to="/movies/new">New Movie</Link></li>
@@ -35,27 +35,30 @@ function ListMovies({moviesApi}) {
     }, []);
 
     if (!movies) {
-        return  <div>Loading...</div>
+        return <div>Loading...</div>
     }
+    return <div>
+        <h1>List movies</h1>
+        {movies.map(m =>
+            <div key={m.title}>
+                <h2>{m.title} ({m.year})</h2>
+                <div>{m.plot}</div>
+            </div>
+        )}
+    </div>;
 }
-function Newmovie({moviesApi}) {
+function NewMovie({moviesApi}) {
     const [title, setTitle] = useState("");
     const [year, setYear] = useState("");
     const [plot, setPlot] = useState("");
 
     const navigate = useNavigate();
-
-
-
-        async function handleSubmit(e) {
-            e.preventDefault();
-            onAddmovie({title, year, plot});
-            await moviesApi.onAddMovie({title, year, plot});
-            navigate("/");
-        }
-
-
-
+    async function handleSubmit(e) {
+        e.preventDefault();
+        onAddMovie({title, year, plot});
+        await moviesApi.onAddMovie({title, year, plot});
+        navigate("/");
+    }
     return <form onSubmit={handleSubmit}>
         <h1>New movie</h1>
         <div>
@@ -68,9 +71,6 @@ function Newmovie({moviesApi}) {
             <label>Plot: <textarea value={plot} onChange={e => setPlot(e.target.value)} /></label>
         </div>
         <button>Submit</button>
-        <pre>
-            {JSON.stringify({title, year, plot})}
-        </pre>
     </form>;
 }
 
@@ -83,13 +83,14 @@ function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage/>}/>
-            <Route path="/movies/new" element={<Newmovie onAddMovie={m => MOVIES.push(m)}/>}/>
+            <Route path="/movies/new" element={<NewMovie onAddMovie={m => MOVIES.push(m)}/>}/>
             <Route path="/movies" element={<ListMovies movies={MOVIES}/>}/>
-            <Route path="/movies/new" element={<Newmovie moviesApi={moviesApi}/>}/>
+            <Route path="/movies/new" element={<NewMovie moviesApi={moviesApi}/>}/>
             <Route path="/movies" element={<ListMovies moviesApi={moviesApi}/>}/>
         </Routes>
     </BrowserRouter>;
 }
+
 ReactDOM.render(
     <Application/>,
     document.getElementById("app")
